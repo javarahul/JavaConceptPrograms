18e64e2fdfcb57a3a70fa13e5fd2c482c6788752
javarahul <tripathi.rahul15@gmail.com>
2022-04-17 20:49:17 +0530

    Create FileWriterClient.java

---
+++ designpattern/behavioural/memento/FileWriterClient.java
@@ -0,0 +1,24 @@
+package javaprograms.designpattern.behavioural.memento;
+
+public class FileWriterClient {
+
+	public static void main(String[] args) {
+		FileWriterCaretaker fileWriterCaretaker = new FileWriterCaretaker();
+
+		FileWriterUtil fileWriterUtil = new FileWriterUtil("data.txt");
+
+		fileWriterUtil.write("First set of data\n");
+
+		System.out.println(fileWriterUtil + "\n\n");
+
+		fileWriterCaretaker.save(fileWriterUtil);
+
+		fileWriterUtil.write("Second set of data\n");
+
+		System.out.println(fileWriterUtil + "\n\n");
+
+		fileWriterCaretaker.undo(fileWriterUtil);
+
+		System.out.println(fileWriterUtil + "\n\n");
+	}
+}
