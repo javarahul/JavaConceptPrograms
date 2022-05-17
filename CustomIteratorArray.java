110885b0a92e1e34b228d3bab7fde2d6a0a3d2f9
javarahul <tripathi.rahul15@gmail.com>
2022-05-17 07:56:33 +0530

    Create CustomIteratorArray.java

---
+++ CustomIteratorArray.java
@@ -0,0 +1,69 @@
+package javaprograms;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class CustomIteratorArray<T> implements Iterable<T> {
+
+	T[] values;
+
+	public CustomIteratorArray(T[] values) {
+		this.values = values;
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		return new ArrayIterator();
+	}
+
+	public T get(int index) {
+		return values[index];
+	}
+
+	public void set(int index, T value) {
+		values[index] = value;
+	}
+
+	public int length() {
+		return values.length;
+	}
+
+	class ArrayIterator implements Iterator<T> {
+		int currIndex = 0;
+
+		@Override
+		public boolean hasNext() {
+			if (currIndex < values.length) {
+				return true;
+			}
+			return false;
+		}
+
+		@Override
+		public T next() {
+			if (!hasNext()) {
+				throw new NoSuchElementException();
+			}
+			return values[currIndex++];
+		}
+
+		@Override
+		public void remove() {
+			if (currIndex > -1) {
+				values[currIndex] = null;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		String[] strings = new String[] { "Hello", "World" };
+		CustomIteratorArray<String> array = new CustomIteratorArray<String>(strings);
+
+		array.set(1, "JavaRahul!");
+
+		Iterator<String> iterator = array.iterator();
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+	}
+}
