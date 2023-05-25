db03771ac22eac44b6621d85f3f97b0604fea1c3
javarahul <tripathi.rahul15@gmail.com>
2023-05-25 08:00:01 +0530

    Create Laptop.java

---
+++ designpattern/creational/factory/factory1/Laptop.java
@@ -0,0 +1,52 @@
+package com.rahul.designpattern.factory.client;
+
+public class Laptop implements Computer {
+
+	private String ram;
+	private String hdd;
+	private String cpu;
+
+	private boolean isGraphicEnabled;
+	private boolean isBluetoothEnabled;
+
+	@Override
+	public String ram() {
+		return this.ram;
+	}
+
+	@Override
+	public String toString() {
+		return "Laptop [ram=" + ram + ", hdd=" + hdd + ", cpu=" + cpu + ", isGraphicEnabled=" + isGraphicEnabled
+				+ ", isBluetoothEnabled=" + isBluetoothEnabled + "]";
+	}
+
+	@Override
+	public String hdd() {
+		return this.hdd;
+	}
+
+	@Override
+	public String cpu() {
+		return this.cpu;
+	}
+
+	@Override
+	public boolean isGraphicEnabled() {
+		return this.isGraphicEnabled;
+	}
+
+	@Override
+	public boolean isBluetoothEnabled() {
+		return this.isBluetoothEnabled;
+	}
+
+	public Laptop(String ram, String hdd, String cpu, boolean isGraphicEnabled, boolean isBluetoothEnabled) {
+		super();
+		this.ram = ram;
+		this.hdd = hdd;
+		this.cpu = cpu;
+		this.isGraphicEnabled = isGraphicEnabled;
+		this.isBluetoothEnabled = isBluetoothEnabled;
+	}
+
+}
